430e8ef37ed2cd1fbedbbcfe27d162ad1e911976
David Ortiz <ortizcdavid@gmail.com>
2021-01-29 14:42:35 -0800

    fixed location

--- cypress/integration/test/login-wallet.sw.js
+++ cypress/integration/test/login-wallet.sw.js
@@ -3,22 +3,22 @@ describe('Log in to Superworld', () => {
   it('logs in to purchase plot of land', () => {
     expect(true).to.equal(true)
     cy.visit('https://superworldappdev.herokuapp.com/')
-    cy.get('#gatsby-focus-wrapper > header > div.container-box > div > div > div.navigation-wrapper > a > span.MuiButton-label').click()
-    cy.wait(2000)
-    cy.contains('BUY REAL ESTATE').click()
-    cy.wait(2000)
+    cy.contains('ENTER SUPERWORLD').click()
+    cy.wait(3000)
     cy.contains('Click Here').click()
   })
 
   it('type into an element to log in', () => {
     //log in to SW real ESTATE
-    cy.get('div.MuiBox-root.jss8').click({force: true})
+    cy.wait(3000)
+    cy.get('.MuiBox-root.jss22').click({force: true})
     .type('superworldqaapp@gmail.com')
-    cy.get('div.MuiBox-root.jss9').click({force: true})
+    cy.get('input[type=password]').click({force: true})
     .type('Testtest1!')
     cy.contains('Sign in').click()
+    cy.wait(5000)
     cy.get('.connect-wallet__cardBtns.connect-wallet__redBtn').click({force: true})
-    cy.wait(10000)
+    cy.wait(5000)
   })
 
   // it('connects MetaMask Notification', () => {
@@ -29,9 +29,11 @@ describe('Log in to Superworld', () => {
 
   it('type into an element to serach for plots of land', () => {
     //Search for plots of land
-    cy.get('.MuiAutocomplete-hasPopupIcon').type('Tulum')
-    cy.contains('Tulum').click({force: true})
-    .end()
+    cy.get('.MuiAutocomplete-hasPopupIcon').type('Mexico City')
+    cy.wait(10000)
+    cy.contains('Mexico City').click({force: true})
+    cy.wait(10000)
+    cy.end()
 
   })
 

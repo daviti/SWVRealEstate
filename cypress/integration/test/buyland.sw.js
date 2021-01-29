23ee62418ae4bc0573b3f9461d29cca9aca9856a
David Ortiz <ortizcdavid@gmail.com>
2021-01-29 14:27:44 -0800

    bought a plot of land success

--- cypress/integration/test/buyland.sw.js
+++ cypress/integration/test/buyland.sw.js
@@ -3,22 +3,22 @@ describe('Log in to Superworld', () => {
   it('logs in to purchase plot of land', () => {
     expect(true).to.equal(true)
     cy.visit('https://superworldappdev.herokuapp.com/')
-    cy.get('#gatsby-focus-wrapper > header > div.container-box > div > div > div.navigation-wrapper > a > span.MuiButton-label').click()
-    cy.wait(3000)
-    cy.contains('BUY REAL ESTATE').click()
+    cy.contains('ENTER SUPERWORLD').click()
     cy.wait(3000)
     cy.contains('Click Here').click()
   })
 
   it('type into an element to log in', () => {
     //log in to SW real ESTATE
-    cy.get('div.MuiBox-root.jss8').click({force: true})
+    cy.wait(3000)
+    cy.get('.MuiBox-root.jss22').click({force: true})
     .type('superworldqaapp@gmail.com')
-    cy.get('div.MuiBox-root.jss9').click({force: true})
+    cy.get('input[type=password]').click({force: true})
     .type('Testtest1!')
     cy.contains('Sign in').click()
+    cy.wait(5000)
     cy.get('.connect-wallet__cardBtns.connect-wallet__redBtn').click({force: true})
-    cy.wait(10000)
+    cy.wait(5000)
   })
 
   // it('connects MetaMask Notification', () => {
@@ -27,15 +27,25 @@ describe('Log in to Superworld', () => {
   //   cy.get('#password').click({force: true})
   // })
 
-  it('type into an element to serach for plots of land', () => {
+  it('type into an element to search for plots of land', () => {
     //Search for plots of land
     cy.get('.MuiAutocomplete-hasPopupIcon').type('Mexico City')
-    cy.contains('Mexico').click({force: true})
-  cy.wait(1000)
-    cy.get('button.mapboxgl-ctrl-icon.mapboxgl-ctrl-zoom-in').click().click().click() .click()
-    .end()
+    cy.wait(10000)
+    cy.contains('Mexico City').click({force: true})
+    cy.wait(10000)
 
+    for(let n = 0; n < 7; n ++) {
+      cy.get('button.mapboxgl-ctrl-icon.mapboxgl-ctrl-zoom-in')
+        .click()
+      }
   })
+
+  it('zoomed to land to buy now', () => {
+    //Search for plots of land
+     cy.get('#view-MapView').click(500, 500, { multiple: true })
+     cy.get('.LoginButton-header').click({force: true})
+  })
+
 
 
 
